70e13d7f93f52adc0a7da3d943b2ef8a69ed2b8c
Kim Lew <kimlew451@gmail.com>
2017-06-02 13:10:44 -0600

    Added .onclick for petsSpan, facesSpan

--- photo_viewer.js
+++ photo_viewer.js
@@ -2,6 +2,9 @@
 function init() {
   var petsSpan = document.getElementById("pets");
   var facesSpan = document.getElementById("faces");
+  
+  petsSpan.onclick = selectPets;
+  facesSpan.onclick = selectFaces;
 }
 
 window.onload = init;
