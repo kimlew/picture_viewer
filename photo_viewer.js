bc3c079cd2e2563e7733a688cc1d475576e4b13a
Kim Lew <kimlew451@gmail.com>
2017-06-02 14:56:14 -0600

    Added comments about class show

--- photo_viewer.js
+++ photo_viewer.js
@@ -20,12 +20,12 @@ function init() {
 
 function selectPets() {
   var ul = document.getElementById("petsList");
-  ul.setAttribute("class", "show");
+  ul.setAttribute("class", "show"); // Does same as: <ul id="petsList" class="show">
 }
 
 function selectFaces() {
   var ul = document.getElementById("facesList");
-  ul.setAttribute("class", "show");
+  ul.setAttribute("class", "show"); // Does same as: <ul id="facesList" class="show">
 }
 
 window.onload = init;
